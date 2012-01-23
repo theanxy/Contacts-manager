32ec62c21afe35434402bc1fad109ba36a8242a0
Wojtek Zając <wojciech@xhtmlized.com>
2012-01-24 00:08:43 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projekt::Application.config.secret_token = '09d9d0f453e1622322541e790a6a08d7e75111916f55a03957cbd502d6d59fceb1dcfd336c9d2c5afef58f042f60251d3e3e1fc852af866b5c8ad1ef1b065253'
