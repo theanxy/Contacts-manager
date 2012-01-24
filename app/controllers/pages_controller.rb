6f370caca1f65e5d6855c780587b36eb1f8e44be
Wojtek Zając <wojciech@xhtmlized.com>
2012-01-24 01:07:44 +0100

    Listing profiles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!, :except => [:home, :about]
+  
   def home
   end
 
   def profile
+    @users = User.order("created_at DESC")
   end
 
   def about
