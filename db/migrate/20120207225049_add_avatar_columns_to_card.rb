7c9113a7e98aad602075b56f2a048526a20ee9d9
Wojtek Zając <wojciech@xhtmlized.com>
2012-02-07 23:53:16 +0100

    Adding working paperclip gem

---
+++ db/migrate/20120207225049_add_avatar_columns_to_card.rb
@@ -0,0 +1,11 @@
+class AddAvatarColumnsToCard < ActiveRecord::Migration
+  def self.up
+    change_table :cards do |t|
+      t.has_attached_file :avatar
+    end
+  end
+
+  def self.down
+    drop_attached_file :cards, :avatar
+  end
+end
