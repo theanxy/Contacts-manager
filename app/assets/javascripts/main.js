e2e5fdab698464b5236516e06694e41af8978d91
Wojtek Zając <wojciech@xhtmlized.com>
2012-02-19 18:39:27 +0100

    Updates to card view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,7 +2,9 @@ $(function() {
 	$('#cards-list').change(function() {
 		$(this).find('option:selected').each(function() {
 			//console.log($(this).attr('name'));
-			window.location.href = "/cards/"+$(this).attr('name');
+			if ($(this).prev().length) {
+				window.location.href = "/cards/"+$(this).attr('name');
+			}
 		})
 	})
 });
--- public/assets/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('#cards-list').change(function() {
-		$(this).find('option:selected').each(function() {
-			//console.log($(this).attr('name'));
-			window.location.href = "/cards/"+$(this).attr('name');
-		})
-	})
-});
