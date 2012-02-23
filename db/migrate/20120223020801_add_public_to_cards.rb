26f145840dcaa921bed780f2860c0debb61fa3b9
Wojtek Zając <wojciech@xhtmlized.com>
2012-02-23 03:15:01 +0100

    Updating migration

--- db/migrate/20120223020801_add_public_to_cards.rb
+++ db/migrate/20120223020801_add_public_to_cards.rb
@@ -1,5 +1,7 @@
 class AddPublicToCards < ActiveRecord::Migration
   def change
     add_column :cards, :public, :integer
+    Card.all.each do |card|
+      card.update_attributes!(:public => 1)
   end
 end
